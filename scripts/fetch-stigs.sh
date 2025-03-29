35c4c798c394132808007526a7c9bc2a711baf95
Neal Fennimore <hi@neal.codes>
2025-03-28 21:03:22 -0400

    Update stigs script to convert to json

--- scripts/fetch-stigs.sh
+++ scripts/fetch-stigs.sh
@@ -1,13 +1,30 @@
 #! /usr/bin/env bash
 
+set -euo pipefail
 shopt -s globstar
 
 if [[ ! -f "data/stigs.zip" ]]; then
     echo "Downloading STIGs..."
     curl -L https://dl.dod.cyber.mil/wp-content/uploads/stigs/zip/U_SRG-STIG_Library_January_2025.zip -o data/stigs.zip
+
     unzip -o data/stigs.zip -d data/stigs
+
+    ARGS=()
+    for archive in data/stigs/*.zip; do
+        ARGS+=("$archive" "${archive%.zip}")
+    done
+
+    parallel -v -N2 unzip -d "{2}" "{1}" ::: "${ARGS[@]}"
+
+    sudo chown -R $USER: data/stigs
+    sudo find data/stigs -type d -exec chmod 755 {} \;
 else
     echo "STIGs already downloaded."
 fi
 
-parallel unzip ::: data/stigs/*.zip
+for file in data/stigs/**/*.xml; do
+    output_file="${file%.xml}.json"
+    if [[ ! -f "$output_file" ]]; then
+        yq --xml-strict-mode -p=xml -o=json <"$file" >"$output_file"
+    fi
+done
