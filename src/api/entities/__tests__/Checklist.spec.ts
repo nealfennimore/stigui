b3a0aab57699d6e854cdec9bddac736f1a736841
Neal Fennimore <neal@neal.codes>
2025-03-31 14:02:27 -0400

    Add in valid schema test

--- src/api/entities/__tests__/Checklist.spec.ts
+++ src/api/entities/__tests__/Checklist.spec.ts
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import { Validator } from 'jsonschema';
 import path from 'path';
 import { Convert } from '../../generated/Stig';
 import { CKLBConverter } from '../Checklist';
@@ -6,6 +7,10 @@ import { CKLBConverter } from '../Checklist';
 
 describe('CKLBConverter', () => {
     const schemaDir = path.join(__dirname, '../../../../public/data/stigs/schema');
+
+    const jsonSchema = JSON.parse(fs.readFileSync(path.join(__dirname, '../../../../data/schema/SV3_CKLB_1_0_JSON_SCHEMA.json'), 'utf-8'));
+    const validator = new Validator();
+
     for (const schema of fs.readdirSync(schemaDir)) {
         it(`should match snapshot for ${schema}`, () => {
             if (schema === "null.json"){
@@ -16,6 +21,12 @@ describe('CKLBConverter', () => {
             const stig = Convert.toStig(data);
             const checklist = CKLBConverter.fromStig(stig, stig.Benchmark.Profile);
             expect(checklist).toMatchSnapshot();
+        });
+
+
+        it(`should validate ${schema}`, ()=>{
+            const data = fs.readFileSync(path.join(schemaDir, schema), 'utf8');
+            expect(validator.validate(data, jsonSchema).valid).toBe(true);
         });
     }
 })
