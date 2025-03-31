eb9e527d2fbe4813d85bd5e7b780218fb7e00413
Neal Fennimore <neal@neal.codes>
2025-03-31 14:01:17 -0400

    Remove null

--- scripts/create-json-stigs.sh
+++ scripts/create-json-stigs.sh
@@ -6,8 +6,11 @@ shopt -s globstar
 mkdir -p public/data/stigs/schema
 
 for file in data/stigs/**/*.xml; do
-    echo $file
     JSON="$(yq --xml-strict-mode -p=xml -o=json <"$file")"
     ID=$(jq -r '.Benchmark.["+@id"]' <<<"$JSON")
+    if [[ $ID == "null" ]]; then
+        echo "No ID found in $file"
+        continue
+    fi
     jq . <<<"$JSON" >"public/data/stigs/schema/$ID.json"
 done
