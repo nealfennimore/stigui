d088c0a03110cb0c3bb313d203fd4d5c83c1ca25
Neal Fennimore <neal@neal.codes>
2025-04-11 22:03:29 -0400

    Remove trailing slash

--- scripts/get-stig-url.sh
+++ scripts/get-stig-url.sh
@@ -3,5 +3,5 @@
 export URL=$(
     curl -s 'https://public.cyber.mil/stigs/compilations/' |
         grep -o -E '"https://dl.dod.cyber.mil/wp-content/uploads/stigs/zip.*" ' |
-        sed 's/"//g'
+        sed -e 's/"//g' -e 's/ $//'
 )
