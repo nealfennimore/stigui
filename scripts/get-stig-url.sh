3b890da2e8d029bb57e6550bc9d9661840f6d78e
Neal Fennimore <neal@neal.codes>
2025-08-01 06:42:53 -0400

    Update get-stig-url.sh

--- scripts/get-stig-url.sh
+++ scripts/get-stig-url.sh
@@ -2,6 +2,6 @@
 
 export URL=$(
     curl -s 'https://public.cyber.mil/stigs/compilations/' |
-        grep -o -E '"https://dl.dod.cyber.mil/wp-content/uploads/stigs/zip.*" ' |
+        grep -o -E '"https://public.cyber.mil/wp-content/uploads/stigs/zip.*" ' |
         sed -e 's/"//g' -e 's/ $//'
 )
